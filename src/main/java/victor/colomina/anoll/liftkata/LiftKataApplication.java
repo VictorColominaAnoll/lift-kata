b9238fc0b25f4ac034fc24ddc018b815bfa38456
Victor Colomina Anoll <victorcolominanoll@gmail.com>
2021-06-04 10:55:08 +0200

    Skeleton

---
+++ src/main/java/victor/colomina/anoll/liftkata/LiftKataApplication.java
@@ -0,0 +1,13 @@
+package victor.colomina.anoll.liftkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/victor/colomina/anoll/liftkata/LiftKataApplicationTests.java
@@ -0,0 +1,13 @@
+package victor.colomina.anoll.liftkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
